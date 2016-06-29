95bb12aa618cc413d05d72df04eaf2df45a4d63d
Lrteeter <lrteeter@gmail.com>
2016-06-29 15:38:40 -0700

    refactored GameDetails

--- frontend/components/GameDetails.jsx
+++ frontend/components/GameDetails.jsx
@@ -72,6 +72,46 @@ var GameDetails = React.createClass({
     this.setState({editing: undefined});
   },
 
+  createCommentField: function() {
+    if (parseInt(this.state.user1.id) === parseInt(this.state.current_user_id)) {
+      var innerHtml = this.state.user2.username + "'s Comments: " + this.state.comments2;
+      var comments =
+        <div>
+          <div className="game-comments">My Comments: {this.state.comments1}</div>
+          <button title="edit" className="edit-game" id={this.state.gameId} onClick={this.editComments}>edit</button>
+          <div className="game-comments">{innerHtml}</div>
+        </div>;
+      var editComments =
+        <form onSubmit={this.handleGameUpdate}>
+          <textarea cols="40" rows="5" valueLink={this.linkState("comments1")}></textarea><br/>
+          <input className="edit-comments-button" type="submit" value="save"/>
+          <div className="game-comments">{innerHtml}</div>
+        </form>;
+    } else {
+      var innerHtml = this.state.user1.username + "'s Comments: " + this.state.comments1;
+      var comments =
+        <div>
+          <div className="game-comments">My Comments: {this.state.comments2}</div>
+          <button title="edit" className="edit-game" id={this.state.gameId} onClick={this.editComments}>edit</button>
+          <div className="game-comments">{innerHtml}</div>
+        </div>;
+      var editComments =
+        <form onSubmit={this.handleGameUpdate}>
+          <textarea cols="40" rows="5" valueLink={this.linkState("comments2")}></textarea><br/>
+          <input className="edit-comments-button" type="submit" value="save"/>
+          <div className="game-comments">{innerHtml}</div>
+        </form>;
+    }
+
+    if (!this.state.editing) {
+      var commentField = comments;
+    } else {
+      var commentField = editComments;
+    }
+
+    return commentField;
+  },
+
   render: function () {
     if (!this.state.gameId) {
       return (<div></div>)
@@ -82,54 +122,22 @@ var GameDetails = React.createClass({
       } else {
         winner = this.state.user2
       }
+      
       if (parseInt(winner.id) === parseInt(this.state.current_user_id)) {
         winner = "me"
       } else {
         winner = winner.username
       }
 
-      if (parseInt(this.state.user1.id) === parseInt(this.state.current_user_id)) {
-        var innerHtml = this.state.user2.username + "'s Comments: " + this.state.comments2;
-        var comments =
-          <div>
-            <div className="game-comments">My Comments: {this.state.comments1}</div>
-            <button title="edit" className="edit-game" id={this.state.gameId} onClick={this.editComments}>edit</button>
-            <div className="game-comments">{innerHtml}</div>
-          </div>;
-        var editComments =
-          <form onSubmit={this.handleGameUpdate}>
-            <textarea cols="40" rows="5" valueLink={this.linkState("comments1")}></textarea>
-            <input className="edit-comments-button" type="submit" value="save"/>
-            <div className="game-comments">{innerHtml}</div>
-          </form>;
-      } else {
-        var innerHtml = this.state.user1.username + "'s Comments: " + this.state.comments1;
-        var comments =
-          <div>
-            <div className="game-comments">My Comments: {this.state.comments2}</div>
-            <button title="edit" className="edit-game" id={this.state.gameId} onClick={this.editComments}>edit</button>
-            <div className="game-comments">{innerHtml}</div>
-          </div>;
-        var editComments =
-          <form onSubmit={this.handleGameUpdate}>
-            <textarea cols="40" rows="5" valueLink={this.linkState("comments2")}></textarea>
-            <input className="edit-comments-button" type="submit" value="save"/>
-            <div className="game-comments">{innerHtml}</div>
-          </form>;
-      }
-
-      if (!this.state.editing) {
-        var commentField = comments;
-      } else {
-        var commentField = editComments;
-      }
-
+      var commentsField = this.createCommentField();
       var time = GameStore.dateToString(this.state.created_at);
+
       return (
         <div className="game-container">
           <h4>{this.state.user1.username} vs. {this.state.user2.username}:</h4>
           <div className="game-winner">Winner: {winner}</div>
-          {commentField}<br/>
+          {commentsField}
+          <br/>
         </div>
       );
     }
